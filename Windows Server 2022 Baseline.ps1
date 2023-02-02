281b2f9f6e4cbe06fcb8cae57e5ae567ed086cf4
eneerge <eneerge@eneerge.net>
2023-02-02 13:54:52 -0600

    ConfigureSMBv1ClientDriver,ConfigureSMBv1server fix

--- Windows Server 2022 Baseline.ps1
+++ Windows Server 2022 Baseline.ps1
@@ -1795,13 +1795,13 @@ function LocalAccountTokenFilterPolicy {
 function ConfigureSMBv1ClientDriver  {
     #18.3.2 => Computer Configuration\Policies\Administrative Templates\MS Security Guide\Configure SMB v1 client driver 
     Write-Info "18.3.2 (L1) Ensure 'Configure SMB v1 client driver' is set to 'Enabled: Disable driver (recommended)"
-    SetRegistry "HKLM:\SYSTEM\CurrentControlSet\Services\mrxsmb1" "Start" "0" $REG_DWORD
+    SetRegistry "HKLM:\SYSTEM\CurrentControlSet\Services\mrxsmb1" "Start" "4" $REG_DWORD
 }
 
 function ConfigureSMBv1server {
     #18.3.3 => Computer Configuration\Policies\Administrative Templates\MS Security Guide\Configure SMB v1 server
     Write-Info "18.3.3 (L1) Ensure 'Configure SMB v1 server' is set to 'Disabled'"
-    SetRegistry "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" "SMB1" "0" $REG_DWORD
+    SetRegistry "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" "SMB1" "4" $REG_DWORD
 }
 
 function DisableExceptionChainValidation {
